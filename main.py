88416fb02782ebeba5630f1086070d7ea1a23833
Marta <48037981+marteczkah@users.noreply.github.com>
2021-08-17 18:32:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+wiki_title = input('What do you want to find on Wikipedia?\n')
+wiki_title = wiki_title.replace(' ', '_')
+url = 'https://en.wikipedia.org/wiki/' + wiki_title
+
+ans = requests.get(url)
+
+if ans.status_code == 404 or ans.status_code == 403:
+    print('Page not found')
+else:
+    soup = BeautifulSoup(ans.text, 'html.parser')
+    about_article = soup.find('div', role='note').text
+    print(about_article)
