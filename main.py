12030eeb8ae88dd436406e674f575ce53eaf04a9
Marta <48037981+marteczkah@users.noreply.github.com>
2021-08-17 18:54:34 +0200

    Added top_report feature

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-wiki_title = input('What do you want to find on Wikipedia?\n')
-wiki_title = wiki_title.replace(' ', '_')
-url = 'https://en.wikipedia.org/wiki/' + wiki_title
-print(url)
+def user_article():
+    wiki_title = input('What do you want to find on Wikipedia?\n')
+    wiki_title = wiki_title.replace(' ', '_')
+    url = 'https://en.wikipedia.org/wiki/' + wiki_title
+    ans = requests.get(url)
+    if ans.status_code == 404 or ans.status_code == 403:
+        print('Page not found')
+    else:
+        soup = BeautifulSoup(ans.text, 'html.parser')
+        about_article = soup.find('div', role='note').text
+        print(about_article)
 
-ans = requests.get(url)
-
-if ans.status_code == 404 or ans.status_code == 403:
-    print('Page not found')
-else:
+def top_report():
+    url = 'https://en.wikipedia.org/wiki/Wikipedia:Top_25_Report'
+    ans = requests.get(url)
     soup = BeautifulSoup(ans.text, 'html.parser')
-    about_article = soup.find('div', role='note').text
-    print(about_article)
+    top_table = soup.find('dl').table.tbody
+    rows = top_table.find_all('tr')
+    for i, row in enumerate(rows):
+        data = row.find_all('td')
+        for index, info in enumerate(data):
+            if index==1:
+                print(i, ': ', info.text)
+                break
+
+user_purpose = input('What do you want to find on Wikipedia? top report/my article\n')
+if user_purpose=='my article':
+    user_article()
+else:
+    top_report()
